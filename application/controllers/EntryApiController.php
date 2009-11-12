4066e048b8de2d4ee8fdc3a74ba6f63c6ff6e725
David Valentiate <me@David-Valentiates-MacBook.local>
2009-11-12 08:52:54 -0500

    fixed putAction method for Entry-API

--- application/controllers/EntryApiController.php
+++ application/controllers/EntryApiController.php
@@ -12,6 +12,17 @@
  */
 class EntryApiController extends Zend_Controller_Action
 {
+    
+    protected static function _mapModelToArray($guestbookEntry) {
+        return array(
+            'id' => $guestbookEntry->getId(),
+            'email' => $guestbookEntry->getEmail(),
+            'created' => $guestbookEntry->getCreated(),
+            'comment' => $guestbookEntry->getComment(),
+        );
+    }
+    
+    
     public function indexAction()
     {
         $modelObj = new Default_Model_GuestbookEntry();
@@ -19,76 +30,58 @@ class EntryApiController extends Zend_Controller_Action
         
         $data = array();
         foreach ($modelSet as $model) {
-            $data[] = array(
-                'id' => $model->getId(),
-                'email' => $model->getEmail(),
-                'created' => $model->getCreated(),
-                'comment' => $model->getComment(),
-            );
+            $data[] = self::_mapModelToArray($model);
         }
         
         $this->view->data = $data;
     }
     
+    
     public function getAction()
     {
-        $id = $this->getRequest()->getQuery('id', null);
-        
-        if ($id === null) {
-            // is this possible? if so either forward, redirect or render index
-        }
-        
         $model = new Default_Model_GuestbookEntry();
-        $model->find($id);
+        $model->find((integer) $this->getRequest()->getParam('id'));
         
-        if ($model->getId() === null) {
-            $this->view->data = array('ok' => false, 'status' => '404');
+        if (null === $model->getId()) {
+            $this->getResponse()->setHttpResponseCode(404);
+            $this->view->data = array('ok' => false, 'status' => 404);
             $this->render();
-            // error, should show 404 error with 404 header, use ErrorControler in some way
-            // doesn't need to be json response, in fact it shouldn't be
-            //$this->_forward();
+            return;
         }
         
-        $this->view->data = array(
-            'id' => $model->getId(),
-            'email' => $model->getEmail(),
-            'created' => $model->getCreated(),
-            'comment' => $model->getComment(),
-        );
+        $this->view->data = self::_mapModelToArray($model);
     }
+    
     
     public function putAction()
     {
         $request = $this->getRequest();
         
-        $id = $request->getQuery('id', null);
-        
-        if ($id === null) {
-            // is this possible? if so either forward, redirect or render index
-        }
-        
         $model = new Default_Model_GuestbookEntry();
-        $model->find($id);
+        $model->find((integer) $request->getParam('id'));
         
-        if ($model->getId() === null) {
-            $this->view->data = array('ok' => false, 'status' => '404');
+        if (null === $model->getId()) {
+            $this->getResponse()->setHttpResponseCode(404);
+            $this->view->data = array('ok' => false, 'status' => 404);
             $this->render();
-            // error, should show 404 error with 404 header, use ErrorControler in some way
-            // doesn't need to be json response, in fact it shouldn't be
-            //$this->_forward();
+            return;
         }
         
         $form = new Default_Form_GuestbookEntry();
         
-        $model->setOptions($form->getValues());
-        
         if (!$form->isValid($request->getPost())) {
-            $this->view->data = array('ok' => false);
+            $this->getResponse()->setHttpResponseCode(409);
+            $this->view->data = array('ok' => false, 'status' => 409);
             $this->render();
+            return;
         }
         
-        $this->getAction();
+        $model->setOptions($form->getValues());
+        $model->save();
+        
+        $this->view->data = self::_mapModelToArray($model);
     }
+    
     
     public function postAction()
     {
@@ -106,17 +99,13 @@ class EntryApiController extends Zend_Controller_Action
             ->setHttpResponseCode(201)
             ->setHeader('Location', $this->view->url(array('action' => 'get', 'id' => $model->getId())));
         
-        $this->view->data = array(
-            'id' => $model->getId(),
-            'email' => $model->getEmail(),
-            'created' => $model->getCreated(),
-            'comment' => $model->getComment(),
-        );
+        $this->view->data = self::_mapModelToArray($model);
     }
     
     public function preDispatch()
     {
         $this->_helper->layout()->disableLayout();
+        // use the custom view json for this api class, might be joined by an xml api later
         $this->_helper->viewRenderer->setRender('json');
         
         // JSON is the proper content type but browsers, currently, make it
