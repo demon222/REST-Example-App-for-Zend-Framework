ab49f2e3732fd1d6415cf2b27149525563265cd4
David Valentiate <me@davidv.ca>
2010-02-07 22:22:24 -0500

    expanding the example data. ACL might need adjustment to have roles of specific be checked against the general

--- scripts/data.sqlite.sql
+++ scripts/data.sqlite.sql
@@ -3,14 +3,20 @@ INSERT INTO user (id, username, name) VALUES (384, 'Dan', 'Dan');
 INSERT INTO entry (comment, creator_user_id, modified) VALUES ("information wants to be free", 384, DATETIME('NOW'));
 
 INSERT INTO user (id, username, name) VALUES (456, 'Alex', 'Alex');
-INSERT INTO entry (id, comment, creator_user_id, modified) VALUES (123, "the dead don't talk much", 456, DATETIME('NOW'));
+INSERT INTO resource_role (user_id, resource, role) VALUES (456, 'Entry', 'member');
 
+INSERT INTO entry (id, comment, creator_user_id, modified) VALUES (123, "the dead don't talk much", 456, DATETIME('NOW'));
 INSERT INTO resource_role (user_id, resource, role) VALUES (456, 'Entry=123', 'owner');
 INSERT INTO permission (resource, role, privilege, permission) VALUES ('Entry=123', 'owner', 'get', 'allow');
 INSERT INTO permission (resource, role, privilege, permission) VALUES ('Entry=123', 'owner', 'put', 'allow');
 INSERT INTO permission (resource, role, privilege, permission) VALUES ('Entry=123', 'owner', 'delete', 'allow');
-
 INSERT INTO permission (resource, role, privilege, permission) VALUES ('Entry=123', 'default', 'get', 'deny');
+
+INSERT INTO entry (id, comment, creator_user_id, modified) VALUES (124, "well of course! but why do you hide so much?", 456, DATETIME('NOW'));
+INSERT INTO resource_role (user_id, resource, role) VALUES (456, 'Entry=124', 'owner');
+INSERT INTO permission (resource, role, privilege, permission) VALUES ('Entry=124', 'owner', 'get', 'allow');
+INSERT INTO permission (resource, role, privilege, permission) VALUES ('Entry=124', 'owner', 'put', 'allow');
+INSERT INTO permission (resource, role, privilege, permission) VALUES ('Entry=124', 'owner', 'delete', 'allow');
 
 INSERT INTO user (id, username, name) VALUES (111, 'Bob', 'Bob');
 INSERT INTO resource_role (user_id, resource, role) VALUES (111, 'Entry', 'admin');
