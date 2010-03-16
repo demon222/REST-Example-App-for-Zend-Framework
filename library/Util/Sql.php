531f35bf939d375a98f2aade04bdfdb78dd99b33
David Valentiate <me@davidv.ca>
2010-03-15 21:39:26 -0400

    fixed bug in integer recognition and improved comments of generateSqlWheresAndParams

--- library/Util/Sql.php
+++ library/Util/Sql.php
@@ -2,25 +2,48 @@
 
 class Util_Sql
 {
+    /**
+     * Where structure takes the form of
+     * $whereStructure = array(
+     *     'space_separated_or_conditions' => $value,
+     *     'space_separated_or_conditions' => $value,
+     *     ...
+     * );
+     *
+     * 'space_separated_or_conditions' are useful for doing searches across
+     * multiple properties for the value.
+     * Example: 'name_first name_last username' => 'Kelly'
+     *
+     * @param array $whereStructure
+     * @param array $validPropertyKeys
+     * @return array('sql' => $andSet, 'params' => $params)
+     */
     public static function generateSqlWheresAndParams($whereStructure, $validPropertyKeys)
     {
         $andSet = array();
         $params = array();
 
         $i = 0;
+        // top level of where structure define conditions that are anded together
         foreach ($whereStructure as $conds => $value) {
+            // space_separated_or_conditions
             $conds = explode(' ', $conds);
 
             $orSet = array();
             foreach ($conds as $cond) {
+                // ensure that the condition specified is a valid property
                 if (!in_array($cond, $validPropertyKeys)) {
                     throw new Rest_Model_BadRequestException($cond . ' is not a valid where property [' . implode(', ', $validPropertyKeys) . ']');
                 }
 
+                // create SQL for supported values types: array, string or integer
                 if (is_array($value) && !empty($value)) {
+
                     $orKeys = array();
                     foreach ($value as $v) {
-                        if (is_string($v) || !is_int($v)) {
+                        // if array of strings or integers, creates a
+                        // 'property IN (:value_0, :value_1, ...)' type of thing
+                        if (is_string($v) || is_int($v)) {
                             $orKeys[] = ':value_' . $i;
                             $params[':value_' . $i] = $v;
                             $i++;
@@ -28,12 +51,14 @@ class Util_Sql
                     }
                     $orSet[] = '"' . $cond . '" IN (' . implode(', ', $orKeys) . ')';
                 } elseif (is_string($value) || is_int($value)) {
+                    // a simple string or an integer, create 'property = :value_0'
                     $params[':value_' . $i] = $value;
                     $orSet[] = '"' . $cond . '" = ' . ':value_' . $i;
                     $i++;
                 }
             }
 
+            // hmm, didn't find something to do, nothing to see here, move along
             if (empty($orSet)) {
                 continue;
             }
