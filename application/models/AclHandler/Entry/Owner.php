06fe6e975217a43e4aaf1badc405e01b791ba4f6
David Valentiate <me@davidv.ca>
2010-03-16 21:40:28 -0400

    unstable: working out entry owner, commit before radical shift in direction

--- application/models/AclHandler/Entry/Owner.php
+++ application/models/AclHandler/Entry/Owner.php
@@ -15,6 +15,7 @@ class Default_Model_AclHandler_Entry_Owner
 
     protected $_roles = array(
         'owner',
+        'admin'
     );
 
     protected $_staticPermissions = array(
@@ -22,6 +23,9 @@ class Default_Model_AclHandler_Entry_Owner
             'deny' => array('get', 'put', 'delete', 'post'),
         ),
         'owner' => array(
+            'allow' => array('get', 'put', 'delete', 'post'),
+        ),
+        'admin' => array(
             'allow' => array('get', 'put', 'delete', 'post'),
         ),
     );
@@ -103,25 +107,29 @@ class Default_Model_AclHandler_Entry_Owner
         $whereAndSet = Util_Sql::generateSqlWheresAndParams($params['where'], $this->getPropertyKeys());
         $sortList = Util_Sql::generateSqlSort($params['sort'], $this->getPropertyKeys());
 
-        if (!$this->getAcl()->isAllowed('owner', 'Entry', 'get')) {
-            return array();
-        }
-
         $sql = ''
-            . ' SELECT resource.id, resource_id AS entry_id, user_id'
-            . ' FROM resource_role AS resource'
-            . ' INNER JOIN entry AS e ON resource.resource_id = e.id'
-            . ' WHERE resource = :generalResource'
-            . ' AND role = :role'
+            // RESOURCE
+            . ' SELECT rr.id, rr.resource_id AS entry_id, rr.user_id'
+            . ' FROM entry AS resource'
+            . ' INNER JOIN resource_role AS rr ON rr.resource_id = resource.id AND rr.role = "owner" AND rr.resource = "Entry"'
+
+            // ACL
+            . $this->_getGenericAclListJoins()
+
+            // ACL
+            . ' WHERE ' . $this->_getGenericAclListWheres()
+
+            // RESOURCE
             . ' AND ' . implode(' AND ', array_merge($whereAndSet['sql'], array('1 = 1')))
             . ' ORDER BY ' . implode(', ', $sortList)
+
             . '';
 
         $query = $this->_getDbHandler()->prepare($sql);
-        $query->execute(array_merge(array(':generalResource' => 'Entry', ':role' => 'owner'), $whereAndSet['param']));
+var_dump($this->_getDbHandler()->errorInfo());
+exit();
+        $query->execute(array_merge($this->_getGenericAclListParams(), $whereAndSet['param']));
         $rowSet = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        return $rowSet;
     }
 
     /**
@@ -260,6 +268,153 @@ class Default_Model_AclHandler_Entry_Owner
         $item['id'] = $id;
 
         return $item;
+    }
+
+    /**
+     * Loops through the roles to check for one that is allowed for the method.
+     *
+     * @param string $method, same as what Zend_Acl referers to as 'privilege' but 'method' used for REST context
+     * @return boolean
+     */
+    public function isAllowed($privilege, array $id = null)
+    {
+        $userId = $this->getAclContextUser();
+
+        $resourceGeneral = $this->getResourceId();
+
+        // first get the possible roles this user has with the resource
+        if (null === $id) {
+            $sql = 'SELECT role FROM resource_role'
+                . ' WHERE user_id = :userId'
+                . ' AND resource = :resourceGeneral'
+                . ' AND resource_id IS NULL'
+                . '';
+            $query = $this->_getDbHandler()->query($sql);
+            $query->execute(array(
+                ':userId' => $userId,
+                ':resourceGeneral' => $resourceGeneral,
+            ));
+        } else {
+            // include roles from specific case
+            $resourceSpecific = $this->getSpecificResourceId($id);
+
+            $sql = 'SELECT role FROM resource_role'
+                . ' WHERE user_id = :userId'
+                . ' AND ('
+                . '     resource = :resourceGeneral'
+                . '     AND ('
+                . '         resource_id IS NULL'
+                . '         OR resource_id = :resourceSpecific'
+                . '     )'
+                . ' )'
+                . '';
+            $query = $this->_getDbHandler()->query($sql);
+            $query->execute(array(
+                ':userId' => $userId,
+                ':resourceGeneral' => $resourceGeneral,
+                ':resourceSpecific' => $resourceSpecific,
+            ));
+        }
+        $rowSet = $query->fetchAll(PDO::FETCH_ASSOC);
+        $roleSet = Util_Array::arrayFromKeyValuesOfSet('role', $rowSet);
+        // make sure 'default' role is in there
+        if (!in_array('default', $roleSet)) {
+            $roleSet[] = 'default';
+        }
+
+        if (null !== $id) {
+            // first check if against this specific resource things are
+            // allowed or denied
+            $roleVarKeyLookup = array();
+            foreach ($roleSet as $index => $role) {
+                $roleVarKeyLookup[':role_' . $index] = $role;
+            }
+
+            $sql = 'SELECT p.id, p.permission, p.privilege, p.resource, p.role'
+                . ' FROM permission AS p'
+                . ' WHERE p.role IN (' . implode(', ', array_keys($roleVarKeyLookup)) . ')'
+                . ' AND p.resource = :resourceGeneral'
+                . ' AND p.resource_id = :resourceSpecific'
+                . ' AND p.privilege = :privilege'
+                . ' ORDER BY p.permission ASC'
+                . '';
+            $query = $this->_getDbHandler()->prepare($sql);
+            $query->execute(array_merge(
+                array(
+                    ':resourceGeneral' => $resourceGeneral,
+                    ':resourceSpecific' => $resourceSpecific,
+                    ':privilege' => $privilege,
+                ),
+                $roleVarKeyLookup
+            ));
+            $row = $query->fetch(PDO::FETCH_ASSOC);
+
+            if (false !== $row) {
+                // able to say that this specific resource is either allowed or denied
+                return $row['permission'] == 'allow';
+            }
+        }
+
+        // specific resource check wasn't definitive, check the general resource
+
+        // add the default role
+        $allowed = false;
+        foreach ($roleSet as $role) {
+            // check if a role is accepted
+            if ($this->getAcl()->isAllowed($role, $resourceGeneral, $privilege)) {
+                // if any role is found that allows, the whole thing allows
+                return true;
+            }
+        }
+
+        // no allows found in the general resource, so permission is denied
+        return false;
+    }
+
+    protected function _getGenericAclListJoins()
+    {
+        $nl = "\n";
+
+        $staticPermissionsSql = '';
+        foreach ($this->_staticPermissions as $role => $permissionSet) {
+            foreach($permissionSet as $permission => $privilegeSet) {
+                if (!in_array('get', $privilegeSet)) {
+                    continue;
+                }
+                $staticPermissionsSql .= 'UNION SELECT NULL, "' . $role . '", "' . $permission . '"' . $nl;
+            }
+        }
+
+        return ''
+            . ' LEFT OUTER JOIN (' . $nl
+            . '     SELECT COALESCE(rr.resource_id, p.resource_id) AS acl_resource_id, min(p.permission) AS acl_permission' . $nl
+            . '     FROM resource_role AS rr' . $nl
+            . '     INNER JOIN (' . $nl
+            . '         SELECT resource_id, role, permission FROM permission' . $nl
+            . '         WHERE resource = :generalResource' . $nl
+            . '         AND privilege = "get"' . $nl
+            . '         ' . $staticPermissionsSql
+            . '     ) AS p ON (rr.role = p.role OR p.role = "default")' . $nl
+            . '     WHERE rr.user_id = :userId' . $nl
+            . '     AND rr.resource = :generalResource' . $nl
+            . '     GROUP BY acl_resource_id' . $nl
+            . ' ) AS acl ON acl_resource_id = resource.id' . $nl
+            . '';
+    }
+
+    protected function _getGenericAclListWheres()
+    {
+        return ''
+            . ' (acl_permission = "allow"' . ($this->isAllowed('get') ? ' OR acl_permission IS NULL' : '') . ')'
+            . '';
+    }
+
+    protected function _getGenericAclListParams()
+    {
+        return array(
+            ':userId' => $this->getAclContextUser(),
+            ':generalResource' => $this->getResourceId(),
+        );
     }
 
     /**
