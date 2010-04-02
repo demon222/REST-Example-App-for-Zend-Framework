daca88455ecda043f7f56244e98a075459e5bc04
David Valentiate <me@davidv.ca>
2010-04-02 17:30:43 -0400

    more refactoring between SimpleTableMap and Standard. Think I've got it right this time. Moved getList and associated function into SimpleTableMap

--- library/Rest/Model/AclHandler/SimpleTableMapAbstract.php
+++ library/Rest/Model/AclHandler/SimpleTableMapAbstract.php
@@ -10,6 +10,139 @@ abstract class Rest_Model_AclHandler_SimpleTableMapAbstract
     protected $_dbTable;
 
     /**
+     * @var string
+     */
+    protected $_resourceName;
+
+    /**
+     * @var string
+     */
+    protected $_defaultListWhere;
+
+    /**
+     * @var string
+     */
+    protected $_defaultListSort;
+
+    /**
+     * @var string
+     */
+    protected $_getListResourceSqlFragment;
+
+    /**
+     * @var integer
+     */
+    protected $_listMaxLength = 100;
+
+    /**
+     * @var number
+     */
+    protected $_listPermissionFilteredRate = 5;
+
+    /**
+     * @param array $params
+     * @return array
+     */
+    public function getList(array $params = null)
+    {
+        $params = is_array($params) ? $params : array();
+
+        if (isset($params['entourage'])) {
+            $entourageHandler = new Default_Model_AclHandler_Entourage($this->getAcl(), $this->getAclContextUser());
+            $data = $entourageHandler->getList(array($this->_resourceName => $params));
+            return $data[$this->_resourceName];
+        }
+
+        if (isset($params['where'])) {
+            // use default properties to search against if none are provided
+            if (!is_array($params['where'])) {
+                $params['where'] = array(implode(' ', $this->_defaultListWhere) => $params['where']);
+            }
+        } else {
+            $params['where'] = array();
+        }
+
+        if (!isset($params['sort']) || !is_array($params['sort'])) {
+            $params['sort'] = $this->_defaultListSort;
+        }
+
+        if (!isset($params['limit']) || 0 >= ((integer) $params['limit'])) {
+            $params['limit'] = $this->_listMaxLength;
+        }
+
+        // this is an optimization. Imposes a slight overhead performance hit,
+        // but for queries where their is very restrictive dependency and a
+        // large resource being looked at, this optimization is very significant
+        if (0 < count($this->_permissionDependency)) {
+            // go through all the dependies and make sure there are WHERE
+            // clauses for them
+            foreach ($this->_permissionDependency as $depResource => $depAssoc) {
+                foreach ($depAssoc as $depId => $resourceId) {
+                    // if there is a WHERE already for the resource id
+                    // associated with the dependency, then can't optimize further
+                    if (in_array($resourceId, array_keys($params['where']))) {
+                        continue;
+                    }
+
+                    $parentResourceHandler = $this->_createAclHandler($depResource);
+                    $list = Util_Array::arrayFromKeyValuesOfSet($depId, $parentResourceHandler->getList());
+
+                    if (0 == count($list)) {
+                        // none of the dependencies, done
+                        return array();
+                    }
+
+                    $params['where'][$resourceId] = $list;
+                }
+            }
+        }
+
+        $whereAndSet = Util_Sql::generateSqlWheresAndParams($params['where'], $this->getPropertyKeys());
+        $sortList = Util_Sql::generateSqlSort($params['sort'], $this->getPropertyKeys());
+
+        $limit = $params['limit'];
+        $dbLimit = floor($limit * $this->_listPermissionFilteredRate);
+
+        $cumulativeRowSet = array();
+        $offset = 0;
+        do {
+            // RESOURCE
+            $sql = $this->_getListResourceSqlFragment;
+
+            $sql .= ''
+                // ACL
+                . $this->_getGenericAclListJoins()
+
+                // ACL
+                . ' WHERE ' . $this->_getGenericAclListWheres()
+
+                // RESOURCE
+                . ' AND ' . implode(' AND ', array_merge($whereAndSet['sql'], array('1 = 1')))
+                . ' ORDER BY ' . implode(', ', $sortList)
+                . ' LIMIT ' . $dbLimit
+                . ' OFFSET ' . $offset
+
+                . '';
+
+            $query = $this->_getDbHandler()->prepare($sql);
+            $query->execute(array_merge($this->_getGenericAclListParams(), $whereAndSet['param']));
+            $rowSet = $query->fetchAll(PDO::FETCH_ASSOC);
+
+            $countUnfiltered = count($rowSet);
+
+            $this->_filterDependenciesNotAllowed($rowSet);
+
+            $cumulativeRowSet += $rowSet;
+
+            $countCumulativeFiltered = count($cumulativeRowSet);
+
+            $offset += $dbLimit;
+        } while ($countCumulativeFiltered < $limit && $countUnfiltered == $dbLimit);
+
+        return array_slice($cumulativeRowSet, 0, $limit);
+    }
+
+    /**
      * @param array $id
      * @return array
      * @throws Rest_Model_NotFoundException
@@ -143,4 +276,60 @@ abstract class Rest_Model_AclHandler_SimpleTableMapAbstract
      * @return Zend_Db_Table_Abstract
      */
     abstract protected function _getDbTable();
+
+    /**
+     * @return string
+     */
+    protected function _getGenericAclListJoins()
+    {
+        $nl = "\n";
+
+        $staticPermissionsSql = '';
+        foreach ($this->_staticPermissions as $role => $permissionSet) {
+            foreach($permissionSet as $permission => $privilegeSet) {
+                if (!is_array($privilegeSet) || (!in_array('get', $privilegeSet) && 0 != count($privilegeSet))) {
+                    continue;
+                }
+                $staticPermissionsSql .= 'UNION SELECT NULL, "' . $role . '", "' . $permission . '"' . $nl;
+            }
+        }
+
+        return ''
+            . ' LEFT OUTER JOIN (' . $nl
+            . '     SELECT COALESCE(rr.resource_id, p.resource_id) AS acl_resource_id, min(p.permission) AS acl_permission' . $nl
+            . '     FROM resource_role AS rr' . $nl
+            . '     INNER JOIN (' . $nl
+            . '         SELECT resource_id, role, permission FROM permission' . $nl
+            . '         WHERE resource = :generalResource' . $nl
+            . '         AND privilege = "get"' . $nl
+            . '         ' . $staticPermissionsSql
+            . '     ) AS p ON (rr.role = p.role OR p.role = "default")' . $nl
+            . '     WHERE rr.user_id = :userId' . $nl
+            . '     AND rr.resource = :generalRoleResource' . $nl
+            . '     GROUP BY acl_resource_id' . $nl
+            . ' ) AS acl ON acl_resource_id = resource.id' . $nl
+            . '';
+    }
+
+    /**
+     * @return string
+     */
+    protected function _getGenericAclListWheres()
+    {
+        return ''
+            . ' (acl_permission = "allow"' . ($this->isAllowed('get') ? ' OR acl_permission IS NULL' : '') . ')'
+            . '';
+    }
+
+    /**
+     * @return array
+     */
+    protected function _getGenericAclListParams()
+    {
+        return array(
+            ':userId' => $this->getAclContextUser(),
+            ':generalResource' => $this->getResourceId(),
+            ':generalRoleResource' => $this->getRoleResourceId(),
+        );
+    }
 }
